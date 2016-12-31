f82d85f435fa5730b0c70bac613f8263db7d9f31
penspinnerqwe <rdwwow@mail.ru>
2016-12-31 16:35:16 +0300

    Added service in a separate file

--- ЛАБА_5-7/AngularApp/lib/main.js
+++ ЛАБА_5-7/AngularApp/lib/main.js
@@ -1,35 +1,5 @@
 var app = angular.module('app', []);
 
-app.service('imageService', function(){
-    return{
-            media: [{
-                    type: "image",
-                    subtype: "photo",
-                    caption: "",
-                    copyright: "ADDLLER",
-                    ["media-metadata"]: [
-                    {
-                        url: "image/210.jpg",
-                        format: "Standard Thumbnail",
-                        height: 75,
-                        width: 75
-                    },
-                    {
-                        url: "image/210.jpg",
-                        format: "mediumThreeByTwo210",
-                        height: 140,
-                        width: 210
-                    },
-                    {
-                        url: "image/210.jpg",
-                        format: "mediumThreeByTwo440",
-                        height: 293,
-                        width: 440
-                    }
-                ]
-            }] 
-};})
-
 app.controller('AppController',function($http,$scope,imageService){
 
             $scope.books = [];
